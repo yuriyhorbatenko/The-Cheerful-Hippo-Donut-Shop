8fe595973c98719418aaa7d9f858ac1c31df8dba
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-05-24 21:46:08 -0400

    deleted example schema, created database folder, created database for schema.sql and began adding products to seeds.sql

---
+++ database/seeds.sql
@@ -0,0 +1,39 @@
+
+
+INSERT INTO orders
+    (menu_item, price, category, purchase)
+VALUES
+    ("The Glazed Hippo", 1.50, "food", false);
+
+INSERT INTO orders
+    (menu_item, price, category, purchase)
+VALUES
+    ("The Chocolate Hippo", 1.50, "food", false);
+
+INSERT INTO orders
+    (menu_item, price, category, purchase)
+VALUES
+    ("The Sprinkled Hippo", 1.50, "food", false);
+
+INSERT INTO orders
+    (menu_item, price, category, purchase)
+VALUES
+    ("The Crazy Hippo", 1.50, "food", false);
+
+INSERT INTO orders
+    (menu_item, price, category, purchase)
+VALUES
+    ("Coffee", 1.50, "drinks", false);
+
+INSERT INTO orders
+    (menu_item, price, category, purchase)
+VALUES
+    ("Iced Coffee", 1.50, "drinks", false);
+
+INSERT INTO orders
+    (menu_item, price, category, purchase)
+VALUES
+    ("Hot Cocoa", 1.50, "drinks", false);
+
+SELECT *
+FROM donutshop_db.orders;
