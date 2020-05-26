00f8fc9ac1b4a16344432f558a8d65e93ee6bae1
Yuriy <xett03@gmail.com>
2020-05-26 19:38:53 -0400

    added html

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var exphbs = require("express-handlebars");
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -33,9 +33,13 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
+app.get("/", function (req, res) {
+  res.render("index");
+})
+
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
