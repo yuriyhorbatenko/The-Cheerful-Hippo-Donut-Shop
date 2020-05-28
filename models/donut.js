b76ae7ab3ad9e0616a011d2411aba2b8b371b2f0
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-05-28 12:49:27 -0400

    added config files orm.js connection.js, added donut.js to models, able to have orders table showing on command

---
+++ models/donut.js
@@ -0,0 +1,24 @@
+//import ORM to create functions that will interact w/database
+var orm = require("../config/orm.js");
+//ORMs selectAll, insertOne,updateOne
+//Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+var donut = {
+  selectAll: function (callback) {
+    orm.selectAll("orders", function (response) {
+      callback(response);
+    });
+  },
+  // variables columnss and valuess are arrays
+  insertOne: function (columns, values, callback) {
+    orm.insertOne("orders", columns, values, function (response) {
+      callback(response);
+    });
+  },
+  updateOne: function (objColVals, condition, callback) {
+    orm.updateOne("orders", objColVals, condition, function (response) {
+      callback(response);
+    });
+  }
+};
+
+module.exports = donut;
