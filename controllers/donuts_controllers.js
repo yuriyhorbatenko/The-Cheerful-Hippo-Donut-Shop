ddeac63583c13cc7e9f61424de58dabd66dfd588
Yuriy <xett03@gmail.com>
2020-06-01 19:37:59 -0400

    new database and menu

--- controllers/donuts_controllers.js
+++ controllers/donuts_controllers.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 // update: put route and return to index
-router.put("/orders/update/:item_id", function (req, res) {
+router.put("/orders/update/:item_id", function(req, res) {
   var condition = "item_id = " + req.params.item_id;
   console.log("condition", condition);
 
@@ -26,8 +26,8 @@ router.put("/orders/update/:item_id", function (req, res) {
       purchase: req.body.purchase
     },
     condition,
-    function (result) {
-      if (result.changedRows == 0) {
+    function(result) {
+      if (result.changedRows === 0) {
         return res.status(404).end();
       } else {
         res.status(200).end();
