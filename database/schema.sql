4f3f9f29a95fb8cee7f792476649f0ac2aa987dd
Yuriy <xett03@gmail.com>
2020-06-01 19:01:01 -0400

    new database and menu

--- database/schema.sql
+++ database/schema.sql
@@ -16,9 +16,10 @@ CREATE TABLE orders
     (100) default " " NOT NULL,
     price decimal
     (16,2) default 0.00 NOT NULL,
-    category varchar
-    (100) default " " NOT NULL,
-    purchase boolean DEFAULT false,
+    food boolean DEFAULT false,
+    drinks boolean DEFAULT false,
+    purchase boolean DEFAULT true,
+    description varchar (200) NULL,
     PRIMARY KEY
     (item_id)
 );
--- database/seeds.sql
+++ database/seeds.sql
@@ -2,79 +2,89 @@
 -- this is the donut types
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("The Glazed Hippo", 1.50, "food", false);
+    ("The Glazed Hippo", 1.50, true, false, "Traditional glazed donut");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("The Chocolate Hippo", 1.50, "food", false);
+    ("The Chocolate Hippo", 1.50, true, false, "Donut with chocolate frosting");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("Sprinkled in Color Hippo", 1.50, "food", false);
+    ("Sprinkled in Color Hippo", 1.50, true, false, "Donut with multi-colored sprinkles");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("Crazy Hippo", 1.50, "food", false);
+    ("Crazy Hippo", 1.50, true, false, "A jelly filled donut with your choice of chocolate, strawberry, or vanilla frosting and apple crumb topping");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("Fancy Pancy Hippo", 1.50, "food", false);
+    ("Fancy Pancy Hippo", 1.50, true, false, "A cruller donut, like a funnel cake but in donut form");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("The Great Hippo", 1.50, "food", false);
+    ("The Great Hippo", 1.50, true, false, "Traditional donut with pumpkin frosting and little pumpkin sprinkles");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("Nutty Hippo", 1.50, "food", false);
+    ("Nutty Hippo", 1.50, true, false, "A donut with chocolate frosting and nuts sprinkled on it");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("Hiccuping Hippo", 1.50, "food", false);
+    ("Hiccuping Hippo", 1.50, true, false, "Traditional donut with vanilla icing topped with pop rocks");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("Old Dirty Hippo", 1.50, "food", false);
+    ("Old Dirty Hippo", 1.50, true, false, "A donut with chocolate frosting, sprinkled with crushed Oreo’s and drizzled with peanut butter");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("Shine Bright Like a Hippo", 1.50, "food", false);
+    ("Shine Bright Like a Hippo", 1.50, true, false, "Donut made with blue Dimond cake topped with vanilla and sprinkled with Dimond sprinkles");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("I Like'em Big, I Like'em Chunky", 1.50, "food", false);
+    ("I Like'em Big, I Like'em Chunky", 1.50, true, false, "A chocolate on chocolate donut with chucks of dark chocolate on top");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("Cinnapotamus", 1.50, "food", false);
+    ("Cinnapotamus", 1.50, true, false, "Cinnamon sugar donut");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("Pineapotamus", 1.50, "food", false);
+    ("Pineapotamus", 1.50, true, false, "Donut with pineapple glaze and sprinkled with chopped pistachios");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("Hippo Wants S'more", 1.50, "food", false);
+    ("Hippo Wants S'more", 1.50, true, false, "Glazed donut filled with marshmallow fluff, glazed with chocolate topped with toasted marshmallow");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, food, purchase, description)
 VALUES
-    ("The Blue Hippo", 1.50, "food", false);
+    ("The Blue Hippo", 1.50, true, false, "Glazed donut with blue frosting and blue sprinkles");
+    
+    INSERT INTO orders
+    (menu_item, price, food, purchase, description)
+VALUES
+    ("Baby Hippos", 4.50, true, false, "these are traditional glazed mini donut holes. You get 15 donut holes");
+    
+    INSERT INTO orders
+    (menu_item, price, food, purchase, description)
+VALUES
+    ("The Hungry Hungry Hippo", 15.00, true, false, "Your choice of 12 donuts");
 
 
 
@@ -87,44 +97,44 @@ VALUES
 --  this is the start of drink orders   -- 
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, drinks, purchase, description)
 VALUES
-    ("Coffee", 1.50, "drinks", false);
+    ("Coffee", 1.50, true, false, "Coffee beans imported from hippotanina, ground and brewed in-house. Has a delicate Mellow sweet taste with a candy");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, drinks, purchase, description)
 VALUES
-    ("Iced Coffee", 1.50, "drinks", false);
+    ("Iced Coffee", 1.50, true, false, "Our coffee blended with ice");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, drinks, purchase, description)
 VALUES
-    ("Hot Cocoa", 1.50, "drinks", false);
+    ("Hot Cocoa", 1.50, true, false, "60% cocoa with whipped topping and chocolate shavings");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, drinks, purchase, description)
 VALUES
-    ("Mint Cocoa", 1.75, "drinks", false);
+    ("Mint Cocoa", 1.75, true, false, "Mint hot chocolate topped with whipped cream and crushed peppermint candy");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, drinks, purchase, description)
 VALUES
-    ("Mochopotamus", 2.00, "drinks", false);
+    ("Mochopotamus", 2.00, true, false, "A cafe mocha with one third espresso and two thirds steamed milk with a portion of chocolate added");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, drinks, purchase, description)
 VALUES
-    ("Bottle Water", 1.00, "drinks", false);
+    ("Bottle Water", 1.00, true, false, "Fiji Water");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, drinks, purchase, description)
 VALUES
-    ("Cappapotamus", 1.50, "drinks", false);
+    ("Cappapotamus", 1.50, true, false, "Single shot of espresso and hot milk with a foamed milk hippo on top");
 
 INSERT INTO orders
-    (menu_item, price, category, purchase)
+    (menu_item, price, drinks, purchase, description)
 VALUES
-    ("Hippaccino", 1.50, "drinks", false);
+    ("Hippaccino", 1.50, true, false, "This is a traditional coffee frappe");
 
 
 
