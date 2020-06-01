2a4b61087b98b3ceda592cab855b34ca3e50f724
Yuriy <xett03@gmail.com>
2020-06-01 19:32:24 -0400

    new database and menu

--- database/schema.sql
+++ database/schema.sql
@@ -12,14 +12,24 @@ CREATE TABLE orders
 (
     item_id int
     AUTO_INCREMENT NOT NULL,
+    
 	menu_item varchar
     (100) default " " NOT NULL,
+    
+    quantity decimal
+    (16) default 1 NOT NULL,
+    
     price decimal
     (16,2) default 0.00 NOT NULL,
+    
     food boolean DEFAULT false,
+    
     drinks boolean DEFAULT false,
+    
     purchase boolean DEFAULT true,
+    
     description varchar (200) NULL,
+
     PRIMARY KEY
     (item_id)
 );
